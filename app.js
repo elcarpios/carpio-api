c4581634a4485eba4707ab08b3800e879d3c0f64
victorcarpiomrf <victor.carpio@marfeel.com>
2018-12-23 19:36:41 +0100

    Whitelist www.carpioproyecto.com domain

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
 const { initRoutes, router } = require('./routes');
 const bodyParser = require('body-parser');
 
@@ -11,6 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cors({ origin: 'http://www.carpioproyecto.com' }));
 app.use('/api', router);
 
 app.listen(port, initRoutes);
